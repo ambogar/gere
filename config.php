ee41b2dc2f1ce048977fa727b86afef0f6c4ae00
ambogar <87851760+ambogar@users.noreply.github.com>
2022-03-02 09:03:39 +0800

    Add files via upload

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * using mysqli_connect for database connection
+ */
+ 
+$databaseHost = 'localhost';
+$databaseName = 'um29xl32_gere';
+$databaseUsername = 'um29xl32_root';
+$databasePassword = '3JJ!z^4b48cP';
+ 
+$conn = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
+ 
+?>
